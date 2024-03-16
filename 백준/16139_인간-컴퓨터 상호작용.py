71371287a14f2fef638bf1c2dcd9470606bceea1
다영 <86220363+imdayoung@users.noreply.github.com>
2024-03-16 19:35:34 +0900

    240316

--- 백준/16139_인간-컴퓨터 상호작용.py
+++ 백준/16139_인간-컴퓨터 상호작용.py
@@ -1,13 +1,28 @@
 import sys
 
 
-S = sys.stdin.readline()
+answer = []
+S = sys.stdin.readline().rstrip()
+len_S = len(S)
 q = int(sys.stdin.readline())
+
+alphabets = [[0 for _ in range(26)] for _ in range(len_S + 1)]
+
+for i in range(len_S):
+    alphabets[i + 1][ord(S[i]) - 97] = alphabets[i][ord(S[i]) - 97] + 1
+    for j in range(26):
+        if j == ord(S[i]) - 97:
+            continue
+        alphabets[i + 1][j] = alphabets[i][j]
 
 for _ in range(q):
     alphabet, l, r = map(str, sys.stdin.readline().split())
+    a = ord(alphabet) - 97
     l = int(l)
     r = int(r)
+    answer.append(alphabets[r + 1][a] - alphabets[l][a])
+
+print(*answer, sep = "\n")
 
 
 # 각 알파벳에 대한 누적합 ..?
