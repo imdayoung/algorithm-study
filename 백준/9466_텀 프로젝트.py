fa40e2846fc8d6ec3a70ed09ff6f05c9d186b019
다영 <86220363+imdayoung@users.noreply.github.com>
2024-04-07 22:00:12 +0900

    240407

--- 백준/9466_텀 프로젝트.py
+++ 백준/9466_텀 프로젝트.py
@@ -1,4 +1,4 @@
-# 17M 42S
+# DFS
 import sys
 sys.setrecursionlimit(10 ** 6)
 
@@ -37,30 +37,30 @@ for _ in range(T):
     print(answer)
 
 
-# 44M 1S
-# 시간초과
+# # BFS
 # import sys
 # from collections import deque
 
 
 # def travel(start):
 #     queue = deque([start])
-#     visited[start] = 0
+#     visited[start] = True
+#     cur_visited = [start]
 
 #     while queue:
 #         std = queue.popleft()
-#         # 탐색 진행 중 마지막 학생이 start 학생을 선택하면 마무리됨
-#         if students[std] == start:
-#             return visited[std] + 1
-        
-#         # 탐색 진행 중 자기 자신을 선택한 학생이 있거나
-#         # 이미 탐색했던 학생을 다시 탐색하게 되면 망한 팀
-#         if std == students[std] or visited[students[std]] != -1:
-#             return -1
-        
-#         # 위의 상황 외에는 계속 탐색을 진행해도 됨
-#         queue.append(students[std])
-#         visited[students[std]] = visited[std] + 1
+#         next_std = students[std]
+#         if visited[next_std]:
+#             if next_std in cur_visited:
+#                 return len(cur_visited[cur_visited.index(next_std):])
+#             else:
+#                 return 0
+#         else:
+#             visited[next_std] = True
+#             queue.append(next_std)
+#             cur_visited.append(next_std)
+
+#     return 0
 
 
 # T = int(sys.stdin.readline())
@@ -68,18 +68,11 @@ for _ in range(T):
 #     n = int(sys.stdin.readline())
 #     students = [0] + list(map(int, sys.stdin.readline().split()))
     
-#     answer = 0
+#     answer = n
+#     visited = [False for _ in range(n + 1)]
 
 #     for i in range(1, n + 1):
-#         # 자기 자신을 선택한 학생
-#         if i == students[i]:
-#             # answer -= 1
-#             continue
-
-#         else:
-#             visited = [-1 for _ in range(n + 1)]
-#             temp = travel(i)
-#             if temp == -1:
-#                 answer += 1
+#         if not visited[i]:
+#             answer -= travel(i)
 
 #     print(answer)
