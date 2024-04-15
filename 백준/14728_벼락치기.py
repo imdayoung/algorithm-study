1a6aa59b635af190fac33cdb69526dff8e89da71
다영 <86220363+imdayoung@users.noreply.github.com>
2024-04-16 00:41:53 +0900

    240415

--- 백준/14728_벼락치기.py
+++ 백준/14728_벼락치기.py
@@ -1,21 +1,20 @@
-# 14M 46S
 import sys
 
 
 # 단원 개수 N, 공부할 수 있는 총 시간 T
 N, T = map(int, sys.stdin.readline().split())
 # 각 단원 별 예상 공부 시간 K, 그 단원 문제의 배점 S
-subjects = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+subjects = [[0, 0]] + [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-dp = [[0 for _ in range(T)] for _ in range(N)]
-for i in range(N):
-    for j in range(T):
-        time = subjects[i][0]
-        point = subjects[i][1]
+dp = [[0 for _ in range(T + 1)] for _ in range(N + 1)]
+for i in range(1, N + 1):
+    time = subjects[i][0]
+    point = subjects[i][1]
 
+    for j in range(1, T + 1):
         if j < time:
             dp[i][j] = dp[i - 1][j]
         else:
             dp[i][j] = max(dp[i - 1][j - time] + point, dp[i - 1][j])
 
-print(dp[-1][-1])
+print(dp[N][T])
--- 백준/14923_미로 탈출.py
+++ 백준/14923_미로 탈출.py
@@ -1,9 +1,41 @@
-# 2M
 import sys
+from collections import deque
 
+
+def bfs(x, y, z):
+    # x좌표, y좌표, 벽 부순 횟수
+    queue = deque([(x, y, z)])
+    visited = [[[-1 for _ in range(2)] for _ in range(M)] for _ in range(N)]
+    visited[x][y][0] = 0
+
+    while queue:
+        x, y, z = queue.popleft()
+        if x == Ex - 1 and y == Ey - 1:
+            return visited[x][y][z]
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx > N - 1 or ny < 0 or ny > M - 1:
+                continue
+                
+            if graph[nx][ny] == 0 and visited[nx][ny][z] == -1:
+                visited[nx][ny][z] = visited[x][y][z] + 1
+                queue.append((nx, ny, z))
+
+            elif graph[nx][ny] == 1 and z == 0:
+                visited[nx][ny][1] = visited[x][y][0] + 1
+                queue.append((nx, ny, 1))
+            
+    return -1
+
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
 N, M = map(int, sys.stdin.readline().split())
 Hx, Hy = map(int, sys.stdin.readline().split())
 Ex, Ey = map(int, sys.stdin.readline().split())
 graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
+print(bfs(Hx - 1, Hy - 1, 0))
