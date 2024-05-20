c42cb643b7006f23e6cfef04cf7ee8f2ab163d0c
imdayoung <dlekdud0102@naver.com>
2024-05-20 17:39:45 +0900

    240520

---
+++ 백준/17396_백도어.py
@@ -0,0 +1,45 @@
+# 14M 39S
+import sys
+import heapq
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        
+        for i in times[now]:
+            cost = distance[now] + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+INF = int(1e9)
+
+N, M = map(int, sys.stdin.readline().split())
+is_visible = list(map(int, sys.stdin.readline().split()))
+is_visible[N - 1] = 0
+
+times = [[] for _ in range(N)]
+
+for _ in range(M):
+    a, b, t = map(int, sys.stdin.readline().split())
+    if is_visible[a] or is_visible[b]:
+        continue
+    times[a].append((b, t))
+    times[b].append((a, t))
+
+distance = [INF for _ in range(N)]
+dijkstra(0)
+
+if distance[N - 1] == INF:
+    answer = -1
+else:
+    answer = distance[N - 1]
+    
+print(answer)
--- 백준/2302_극장 좌석.py
+++ 백준/2302_극장 좌석.py
@@ -1,4 +1,3 @@
-# 21M 53S
 import sys
 
 
