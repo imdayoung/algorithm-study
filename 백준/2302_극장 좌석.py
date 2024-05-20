96bbb2d93f89289ad3c0636676d7e7e44fa916e8
imdayoung <dlekdud0102@naver.com>
2024-05-20 17:09:57 +0900

    240520

--- 백준/2302_극장 좌석.py
+++ 백준/2302_극장 좌석.py
@@ -1,4 +1,4 @@
-# 17M
+# 21M 53S
 import sys
 
 
@@ -6,29 +6,17 @@ answer = 1
 
 N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
-vips = [int(sys.stdin.readline()) for _ in range(M)]
+vips = [0] + [int(sys.stdin.readline()) for _ in range(M)] + [N + 1]
 
 if N == 1:
     answer = 1
-    
-elif N == 2:
-    if M == 1 or M == 2:
-        answer = 1
-
-    else:
-        answer = 2
 else:
-    dp = [0 for _ in range(N + 1)]
+    dp = [1 for _ in range(N + 1)]
     dp[1] = 1
-    dp[2] = 2
-    for i in range(3, N + 1):
+    for i in range(2, N + 1):
         dp[i] = dp[i - 1] + dp[i - 2]
 
-    temp = [0]
-    end = N + 1
-
     for i in range(M + 1):
-        print(dp[temp[i + 1] - temp[i] - 1])
-        answer *= dp[temp[i + 1] - temp[i] - 1]
+        answer *= dp[vips[i + 1] - vips[i] - 1]
 
 print(answer)
