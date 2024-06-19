ac7f1bb7c0b1ccd8ae39cd081b395f2d40cea37e
다영 <86220363+imdayoung@users.noreply.github.com>
2024-06-19 22:32:54 +0900

    240619

---
+++ 백준/ing/1405_미친 로봇.py
@@ -0,0 +1,4 @@
+N, east, west, south, north = map(int, input().split())
+print(N, east, west, south, north)
+# 로봇이 같은 곳을 한 번보다 많이 이동하지 않을 때 단순
+
--- 백준/ing/22857_가장 긴 짝수 연속한 부분.py
+++ 백준/ing/22857_가장 긴 짝수 연속한 부분.py
@@ -2,5 +2,14 @@ import sys
 
 
 N, K = map(int, sys.stdin.readline().split())
-nums = list(map(int, sys.stdin.readline().split()))
+nums = [0] + list(map(int, sys.stdin.readline().split()))
 
+dp = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+for i in range(1, N + 1):
+    for j in range(K + 1):
+        if nums[i] % 2 == 0:
+            dp[i][j] = dp[i - 1][j] + 1
+        if j != 0 and nums[i] % 2 == 1:
+            dp[i][j] = dp[i - 1][j - 1]
+
+# for i in range()
